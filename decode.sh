f6ea9e2f778e648256dc44a2d867c514a39e28ff
Yifan Yang <64255737+yfyeung@users.noreply.github.com>
2023-10-06 14:50:58 +0800

    Update decode.sh

--- decode.sh
+++ decode.sh
@@ -2,11 +2,11 @@ export CUDA_VISIBLE_DEVICES=
 
 for epoch in {20..40}; do
   for ((avg=1; avg<=$epoch-20; avg++)); do
-    ./pruned_transducer_stateless7/decode.py \
+    ./zipformer/decode.py \
       --epoch $epoch \
       --avg $avg \
-      --exp-dir ./pruned_transducer_stateless7/exp_960 \
-      --max-duration 600 \
+      --exp-dir ./zipformer/exp \
+      --max-duration 2000 \
       --decoding-method greedy_search
   done
 done
