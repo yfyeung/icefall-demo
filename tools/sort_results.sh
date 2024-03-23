f62f255044bf70b3552679475dde8726a0cdb643
Yifan Yang <64255737+yfyeung@users.noreply.github.com>
2024-03-23 16:46:36 +0800

    Update sort_results.sh

--- tools/sort_results.sh
+++ tools/sort_results.sh
@@ -1,4 +1,4 @@
 cd exp/greedy_search
 grep greedy_search wer-summary-test-clean-greedy_search-epoch-* | awk '{print $2, $1}' | sort -n > 1
 grep greedy_search wer-summary-test-other-greedy_search-epoch-* | awk '{print $2, $1}' | sort -n > 2
-python sort.py > 3
+python sort_results.py > 3
